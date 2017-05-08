2bfd29dca2b66411b8d495a0cd58c73de677a6ed
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-08 10:05:41 -0600

    file clean-up

---
+++ src/containers/index.js
@@ -0,0 +1,3 @@
+export { Main } from './Main';
+export { Welcome } from './Welcome';
+export { CurrentWorkout } from './currentWorkout';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Welcome } from './containers/Welcome';
-import { Main } from './containers/Main';
+import { Welcome, Main } from './containers';
 
 export class Root extends Component {
   constructor(){
